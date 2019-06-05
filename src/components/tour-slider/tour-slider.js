b002f0183a679069c085962a096992567bf44eda
unknown <saint2laurent@gmail.com>
2019-06-05 15:51:31 +0300

    Slider updates

--- src/components/tour-slider/tour-slider.js
+++ src/components/tour-slider/tour-slider.js
@@ -7,9 +7,13 @@ class TourSlider extends Component {
     constructor(props) {
         super(props);
         this.imageDiv= React.createRef()
+        this.spinnerInterval = {}
+        this.spinnerTimeout = {}
+
         this.state = {
 
             sliderIndex: 0,
+            sliderPercentage: 30,
             sliderTrips: [
                 {
                     id: 1,
@@ -32,21 +36,55 @@ class TourSlider extends Component {
                     text2: 'Lorem',
                     text3: 'Lorem Ipsum'
                 }
-            ]
+            ],
+
         }
-
+        this.increaseSliderIndex = this.increaseSliderIndex.bind(this)
         this.setActiveIndex = this.setActiveIndex.bind(this)
-
+        this.getRemainingPercentage = this.getRemainingPercentage.bind(this)
+        this.increaseSliderInterval = this.increaseSliderInterval.bind(this)
     }
 
+
+    componentDidMount() {
+        this.spinnerTimeout = setInterval(this.increaseSliderIndex, 3000)
+    }
+
+    increaseSliderIndex(){
+        console.log('increasing index')
+        if(this.state.sliderIndex < this.state.sliderTrips.length-1){
+            this.setActiveIndex(this.state.sliderIndex + 1)
+        }else{
+            this.setActiveIndex(0)
+        }
+        // clearInterval(this.spinnerInterval)
+        // this.setState({sliderPercentage: 0})
+        // this.spinnerInterval = setInterval(this.increaseSliderInterval, 100)
+    }
 
     setActiveIndex(i){
-        console.log(this.state.sliderTrips[i])
         this.imageDiv.current.style.background = 'url(' + this.state.sliderTrips[i].imageUrl + ') no-repeat center center';
         this.imageDiv.current.style.backgroundSize = 'cover'
+        this.setState({sliderIndex: i})
     }
 
+    increaseSliderInterval(){
+        this.setState({sliderPercentage: this.state.sliderPercentage + 1})
+    }
+
+    getRemainingPercentage(i){
+        console.log('called')
+        if(i !== this.state.sliderIndex){
+            return 0
+        }else{
+            return 20
+        }
+    }
+
+
+
     render() {
+
         return (
             <div className='row p-0 m-0'>
                 <div className='col-12 p-0 m-0 image-header-container'>
@@ -55,11 +93,10 @@ class TourSlider extends Component {
                     </div>
                     <div className='progress-spinner-container'>
                         <ul className='list-unstyled ml-4 mb-3'>
-
                             {this.state.sliderTrips.map((trip, i)=>{
                                 return (
                                     <li key={trip.id} onClick={()=>{this.setActiveIndex(i)}}>
-                                        <CircularProgressbar  strokeWidth={15} circleRatio={1} className='progress-spinner' value={90} styles={buildStyles({pathColor: '#3e98c7', trailColor: 'rgba(0,0,0,.8)'})} />
+                                        <CircularProgressbar  strokeWidth={15} circleRatio={1} className='progress-spinner' value={i === this.state.sliderIndex ? this.state.sliderPercentage : 0} styles={buildStyles({pathColor: '#3e98c7', trailColor: 'rgba(0,0,0,.8)'})} />
                                     </li>
                                 )
                             })}
