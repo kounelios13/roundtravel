5cb80dac1859dd5a4383df72a39926a712312ec8
Saint Laurent <saint2laurent@gmail.com>
2019-06-05 03:41:18 +0300

    Initial commit

---
+++ src/components/tour-slider/tour-slider.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class ImageHeader extends Component {
+    render() {
+        return (
+            <div className='row p-0 m-0'>
+                <div className='col-12 p-0 m-0 image-header-container'>
+                    <div className='image-header-image'></div>
+                    <div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ImageHeader;
