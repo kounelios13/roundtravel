b03d21a68710dedeeea5a1095109746941f6da07
Saint Laurent <saint2laurent@gmail.com>
2019-06-05 04:28:55 +0300

    Slider update

--- src/components/tour-slider/tour-slider.js
+++ src/components/tour-slider/tour-slider.js
@@ -13,45 +13,53 @@ class TourSlider extends Component {
             sliderTrips: [
                 {
                     id: 1,
-                    imageUrl: '../src/assets/images/1.jpg',
+                    imageUrl: 'https://cdn.sooribali.com//img/site_images/soori-bali-beachfront-infinity-pool.jpg',
                     text1: 'Bali Tour',
                     text2: 'Lorem',
                     text3: 'Lorem Ipsum'
                 },
                 {
                     id: 2,
-                    imageUrl: '../src/assets/images/2.jpg',
+                    imageUrl: 'https://images.thrillophilia.com/image/upload/s--DGqAXu3R--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/110/247/original/1491907892_krabi.jpg.jpg?1491907892',
                     text1: 'Bali Tour',
                     text2: 'Lorem',
                     text3: 'Lorem Ipsum'
                 },
                 {
                     id: 3,
-                    imageUrl: '../src/assets/images/3.jpg',
+                    imageUrl: 'https://cdn.shopify.com/s/files/1/1003/7610/products/night_scene_of_the_Louvre_Museum_Wall_Mural_Wallpaper_a.jpg?v=1527599727',
                     text1: 'Bali Tour',
                     text2: 'Lorem',
                     text3: 'Lorem Ipsum'
                 }
             ]
         }
+
+        this.setActiveIndex = this.setActiveIndex.bind(this)
+
     }
 
 
+    setActiveIndex(i){
+        console.log(this.state.sliderTrips[i])
+        this.imageDiv.current.style.background = 'url(' + this.state.sliderTrips[i].imageUrl + ') no-repeat center center';
+        this.imageDiv.current.style.backgroundSize = 'cover'
+    }
 
     render() {
         return (
             <div className='row p-0 m-0'>
-                <div ref={this.imageDiv} className='col-12 p-0 m-0 image-header-container'>
-                    <div className='image-header-image'>
+                <div className='col-12 p-0 m-0 image-header-container'>
+                    <div className='image-header-image' ref={this.imageDiv}>
 
                     </div>
                     <div className='progress-spinner-container'>
                         <ul className='list-unstyled ml-4 mb-3'>
 
-                            {this.state.sliderTrips.map((trip)=>{
+                            {this.state.sliderTrips.map((trip, i)=>{
                                 return (
-                                    <li key={trip.id}>
-                                        <CircularProgressbar  strokeWidth={15} circleRatio={1} className='progress-spinner' value={50} styles={buildStyles({pathColor: '#3e98c7', trailColor: 'rgba(0,0,0,.8)'})} />
+                                    <li key={trip.id} onClick={()=>{this.setActiveIndex(i)}}>
+                                        <CircularProgressbar  strokeWidth={15} circleRatio={1} className='progress-spinner' value={90} styles={buildStyles({pathColor: '#3e98c7', trailColor: 'rgba(0,0,0,.8)'})} />
                                     </li>
                                 )
                             })}
