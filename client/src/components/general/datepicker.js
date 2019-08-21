d8d0a51400f3fd4c60018567e5863832de0d5183
unknown <saint2laurent@gmail.com>
2019-08-21 15:44:47 +0300

    Tour pricing, UX Improvements, component reorganization

--- client/src/components/general/datepicker.js
+++ client/src/components/general/datepicker.js
@@ -6,13 +6,22 @@ import '../../styles/datepicker.scss'
 
 class Datepicker extends Component {
 
+  constructor(props) {
+    super(props)
+    this.handleDayClick = this.handleDayClick.bind(this);
+  }
+
+  handleDayClick(day) {
+    this.setState({ selectedDay: day }, ()=>{
+      this.props.updateDisplayedDate(day)
+    });
+  }
+
+
+
   render() {
     return (
-      <div className='my-5 box shadow border-top-primary p-4'>
-        <div className="col-12">
-          <h3 className='display-7-2 text-normal m-0 mb-2'>Αναχωρησεις <span className="text-small text-secondary">(14)</span></h3>
-        </div>
-
+      <div className='box col-12'>
         <DayPicker
           locale={'en/gr'}
           months={['Ιανουαριος', 'Φευρουαριος', 'Μαρτιος', 'Απριλιος', 'Μαιος', 'Ιουνιος', 'Ιουλιος', 'Αυγουστος', 'Σεπτεμβριος', 'Οκτοβριος', 'Νοεμβριος', 'Δεκεμβριος']}
@@ -20,6 +29,7 @@ class Datepicker extends Component {
           weekdaysShort={['Δε', 'Τρ', 'Τε', 'Πε', 'Πα', 'Σα', 'Κυ']}
           labels={{nextMonth: 'Επομενος μηνας', previousMonth: 'Προηγουμενος μηνας'}}
           modifiersStyles={this.modifiersStyles}
+          onDayClick={this.handleDayClick}
           selectedDays={[
             new Date(2019, 7, 23),
             new Date(2019, 7, 10),
